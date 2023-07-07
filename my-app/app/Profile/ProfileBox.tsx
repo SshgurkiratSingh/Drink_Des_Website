6e27358eac4256d02e1f245a56971529115091ce
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-07-07 08:20:42 +0530

    feat:schema

--- my-app/app/Profile/ProfileBox.tsx
+++ my-app/app/Profile/ProfileBox.tsx
@@ -1,8 +1,8 @@
 "use client";
-
 import React from "react";
 import Button from "../components/Button";
 import { SafeUser } from "../types";
+import Collapse from "../components/Collapse";
 
 interface ProfileBoxProps {
   currentUser: SafeUser;
@@ -10,23 +10,41 @@ interface ProfileBoxProps {
 
 const ProfileBox: React.FC<ProfileBoxProps> = ({ currentUser }) => {
   const userImage = currentUser.image || "/images/placeholder.png";
+
   return (
-    <div className="flex justify-center items-center m-2">
-      <figure className="snip1344">
-        <img src={userImage} alt="profile-pic" className="background" />
-        <img src={userImage} alt="profile-sample6" className="profile" />
-        <figcaption>
-          <h3>
-            {currentUser.name}
-            <span>Balance :Rs.{currentUser.balance}</span>
-          </h3>
-          <div className="flex flex-row items-center justify-center ">
-            <Button onClick={() => {}} s4 label="Update Profile Picture" />
-            <Button onClick={() => {}} s4 label="Update Age, Body ,Weight" />
-          </div>
-        </figcaption>
-      </figure>
-    </div>
+    <>
+      {!currentUser.age ||
+      !currentUser.Height ||
+      !currentUser.weight ||
+      !currentUser.gender ? (
+        <Collapse
+          title="Alert"
+          content="Please Update Your Profile Detail (Age,Weight,Gender,Height)"
+        />
+      ) : (
+        <></>
+      )}
+      <div className="flex justify-center items-center m-2">
+        <figure className="snip1344">
+          <img src={userImage} alt="profile-pic" className="background" />
+          <img src={userImage} alt="profile-sample6" className="profile" />
+          <figcaption>
+            <h3>
+              {currentUser.name}
+              <span>Balance :Rs.{currentUser.balance}</span>
+            </h3>
+            <div className="flex flex-row items-center justify-center p-2 m-2">
+              <Button onClick={() => {}} s4 label="Update Profile Picture" />
+              <Button
+                onClick={() => {}}
+                s4
+                label="Modify age, height, weight, and gender"
+              />
+            </div>
+          </figcaption>
+        </figure>
+      </div>
+    </>
   );
 };
 
