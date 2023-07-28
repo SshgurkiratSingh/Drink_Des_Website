ad2c4e197a0856c9cee7450a61d5cda1459fcfac
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-07-28 07:39:06 +0530

    Update useNotificationModal.tsx

--- app/hooks/useNotificationModal.tsx
+++ app/hooks/useNotificationModal.tsx
@@ -4,22 +4,12 @@ interface NotificationModalStore {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
-  totalAlert: number;
-  setTotalAlert: (value: number) => void;
-  incrementTotalAlert: () => void;
-  decrementTotalAlert: () => void;
 }
 
 const useNotificationModal = create<NotificationModalStore>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
-  totalAlert: 0,
-  setTotalAlert: (value) => set({ totalAlert: value }),
-  incrementTotalAlert: () =>
-    set((state) => ({ totalAlert: state.totalAlert + 1 })),
-  decrementTotalAlert: () =>
-    set((state) => ({ totalAlert: state.totalAlert - 1 })),
 }));
 
 export default useNotificationModal;
